cfa00bc525a0da780a0c4a97502fcacb260f62e8
Bijan Dayani <bdayani518@gmail.com>
2019-11-27 23:19:28 -0500

    friendsData created

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friendsData = [
+    {
+        name: "Ryan McPoyle",
+        photo: "https://static.tvmaze.com/uploads/images/medium_portrait/204/511592.jpg",
+        scores: [3, 5, 1, 2, 4, 5, 5, 3, 1, 2]
+    },
+    {
+        name: "Liam McPoyle",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0F-W5Dc0dMe457mRCRMhYB21UAbCXy2tQ70nFsqwTLewimihs&s",
+        scores: [5, 2, 3, 4, 1, 3, 5, 2, 1, 3]
+    },
+    {
+        name: "Margaret McPoyle",
+        photo: "https://static.tvmaze.com/uploads/images/medium_portrait/174/435698.jpg",
+        scores: [1, 2, 2, 1, 3, 4, 1, 1, 4, 2]
+    },
+    {
+        name: "Prince 'He-Man' Adam",
+        photo: "https://i.pinimg.com/originals/6d/1a/04/6d1a04dd2f86a0e3c8ccab2ff0800ef7.jpg",
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    }
+];
